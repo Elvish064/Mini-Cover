32da90f9f88221f6f8b88c782d7b3431acb7b017
Elvish064 <elvish064@qq.com>
2025-11-30 18:11:24 +0800

    fix:update api

--- src/components/CoverGenerator.vue
+++ src/components/CoverGenerator.vue
@@ -537,7 +537,7 @@ export default {
     },
     async getRandomBgImage() {
       try {
-        const response = await fetch('https://api.elvish.me/pic');
+        const response = await fetch('https://pic.elvish.me/pc');
         if (!response.ok) {
           throw new Error('获取随机图片失败');
         }
